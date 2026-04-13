1ccf315d1235eccc2940a7fce62e917326fa2591
viktor-mouton <viktor.mouton@gmail.com>
2026-04-13 20:47:44 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white">
+      <h1 className="text-4xl font-bold text-gray-900">Hello from site-template</h1>
+    </main>
+  )
+}
